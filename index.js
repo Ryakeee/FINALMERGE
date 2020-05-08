55ff2be927b4e3c17c608d36cd5455f08fb8d762
Ryakeee <61469562+Ryakeee@users.noreply.github.com>
2020-05-08 22:12:50 +0800

    test push

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
+
 //Import Models
 const collegeModel = require('./models/college');
 const professorModel = require('./models/professor');
